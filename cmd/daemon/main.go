65c2ce31968949ff4f0ed814dbaaabb996cb0f9f
Vyacheslav Lyagusha <vlyagusha@gmail.com>
2022-05-02 11:19:02 +0300

    Dockerfile added, config file name in flags

--- cmd/daemon/main.go
+++ cmd/daemon/main.go
@@ -11,16 +11,20 @@ import (
 	internalgrpc "github.com/vlyagusha/system_stats_daemon/internal/server/grpc"
 )
 
-var port string
+var (
+	port       string
+	configFile string
+)
 
 func init() {
-	flag.StringVar(&port, "port", "50005", "port")
+	flag.StringVar(&port, "port", "50005", "Daemon port")
+	flag.StringVar(&configFile, "config", "configs/config.yaml", "Path to configuration file")
 }
 
 func main() {
 	flag.Parse()
 
-	cfg, err := config.LoadConfig("configs/config.yaml")
+	cfg, err := config.LoadConfig(configFile)
 	if err != nil {
 		log.Fatalf("failed to load config: %s", err)
 	}
